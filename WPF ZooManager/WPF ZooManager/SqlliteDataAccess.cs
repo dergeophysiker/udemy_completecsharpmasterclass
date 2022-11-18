4b583b6b0e0c6bad2a08dc2bdaeec5671d3b464d
Brendan Sullivan <brendans@chevron.com>
2022-11-17 23:02:01 -0600

    newest work

---
+++ WPF ZooManager/WPF ZooManager/SqlliteDataAccess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Data;
+using Dapper;
+
+namespace WPF_ZooManager
+{
+    class SqlliteDataAccess
+    {
+
+        public static string LoadPeople()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                //var output = cnn.Query<string>("select * from Person", new DynamicParameters());
+                var output1 = cnn.QueryFirstOrDefault<string>("SELECT FirstName FROM Person", new { Id = 1 });
+                return output1;
+            }
+
+
+        }
+
+  
+
+
+        private static string LoadConnectionString(string id="Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+
+    }
+}
